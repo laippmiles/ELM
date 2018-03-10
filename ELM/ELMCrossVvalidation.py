08860f0ee0ad4b7463809db130ea9480b21d12ee
laipp123 <laippmiles@gmail.com>
2018-03-10 14:31:43 +0800

    ELM

--- ELM/ELMCrossVvalidation.py
+++ ELM/ELMCrossVvalidation.py
@@ -1,23 +1,29 @@
 from WELM import *
 from ELM import *
-testnum = 5
-totalAcc = 0
-totalGmean = 0
-totalTrainTime = 0
-for i in range(testnum):
-    k = i+1
-    acc, gmean, Rn, trainTime = WELM('WWP509_2017',50, C=64,i=k)
-    #acc, gmean, Rn, trainTime = ELM('WWP509_2017', 145,  i=k)
-    totalAcc += acc
-    totalGmean += gmean
-    totalTrainTime += trainTime
-    if i == 0 :
-        totalRn = Rn
-    else:
-        totalRn += Rn
+def ELMCrossVvalidation(numberofHiddenNeurons = 50, C = 64):
+    CrossVvalidatioNum = 5
+    totalAcc = 0
+    totalGmean = 0
+    totalTrainTime = 0
+    for i in range(CrossVvalidatioNum):
+        k = i+1
+        acc, gmean, Rn, trainTime = WELM('WWP509_2017',numberofHiddenNeurons, C,i=k)
+        #acc, gmean, Rn, trainTime = ELM('WWP509_2017', 145,  i=k)
+        totalAcc += acc
+        totalGmean += gmean
+        totalTrainTime += trainTime
+        if i == 0 :
+            totalRn = Rn
+        else:
+            totalRn += Rn
 
-print('totalAcc:',totalAcc/testnum)
-print('totalGmean:',totalGmean/testnum)
-print('totalTrainTime:',totalTrainTime/testnum)
-for i in range(list(shape(totalRn))[0]):
-        print('totalR', i+1 ,':',totalRn[i]/testnum)
+    totalAcc /= CrossVvalidatioNum
+    totalGmean /= CrossVvalidatioNum
+    totalTrainTime /= CrossVvalidatioNum
+    print('totalAcc:',totalAcc)
+    print('totalGmean:',totalGmean)
+    print('totalTrainTime:',totalTrainTime)
+    for i in range(list(shape(totalRn))[0]):
+            totalRn[i] /= CrossVvalidatioNum
+            print('totalR', i+1 ,':',totalRn[i])
+    return totalAcc, totalGmean, totalTrainTime, totalRn
---
+++ ELM/aWholeTest.py
@@ -0,0 +1,22 @@
+from ELMCrossVvalidation import ELMCrossVvalidation
+from numpy import shape
+wholeTestNum = 10
+acc = 0
+gmean = 0
+trainTime = 0
+
+for i in range(wholeTestNum):
+    totalAcc, totalGmean, totalTrainTime, totalRn = ELMCrossVvalidation(20,64)
+    if i ==0 :
+        Rn = totalRn
+    else:
+        Rn += totalRn
+    acc +=totalAcc
+    gmean += totalGmean
+    trainTime += totalTrainTime
+
+print('-'*10,'Acc:',acc/wholeTestNum,'-'*10)
+print('-'*10,'Gmean:',gmean/wholeTestNum,'-'*10)
+print('-'*10,'TrainTime:',trainTime/wholeTestNum,'-'*10)
+for i in range(list(shape(Rn))[0]):
+    print('totalR', i + 1, ':', Rn[i] / wholeTestNum)
